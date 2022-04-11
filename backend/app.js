81fa96fb9d10ce695c08512f4b3a920b2ea6de32
soumyadipmanna <cse1898@cemk.ac.in>
2022-04-11 22:06:38 +0530

    Order Model Completed

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,15 @@
 const express = require('express');
 const connect = require('./connection');
 const cors = require('cors');
-
+const orderRouter = require('../backend/routes/orderRouters');
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
+app.use('/api', orderRouter);
+
 connect();
 
-
-app.get('/',(req,res)=>{
-    res.status(201).json({
-        message:'Its working'
-    });
-});
 
 module.exports = app;
